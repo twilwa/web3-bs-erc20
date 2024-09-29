94ed24b4b90a299a809d9fac06bc6a5c46c4e571
twilwa <spywarezzz@proton.me>
2024-09-28 20:46:01 -0700

    Initial commit with 🏗️ Scaffold-ETH 2

---
+++ packages/foundry/script/Deploy.s.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "../contracts/YourContract.sol";
+import "./DeployHelpers.s.sol";
+import "../contracts/SE2Token.sol";
+
+contract DeployScript is ScaffoldETHDeploy {
+  error InvalidPrivateKey(string);
+
+  function run() external {
+    uint256 deployerPrivateKey = setupLocalhostEnv();
+    if (deployerPrivateKey == 0) {
+      revert InvalidPrivateKey(
+        "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
+      );
+    }
+    vm.startBroadcast(deployerPrivateKey);
+
+    YourContract yourContract = new YourContract(vm.addr(deployerPrivateKey));
+    console.logString(
+      string.concat(
+        "YourContract deployed at: ", vm.toString(address(yourContract))
+      )
+    );
+
+    vm.stopBroadcast();
+
+    vm.startBroadcast(deployerPrivateKey);
+
+    SE2Token se2Token = new SE2Token();
+    console.logString(
+      string.concat("SE2Token deployed at: ", vm.toString(address(se2Token)))
+    );
+
+    vm.stopBroadcast();
+
+    /**
+     * This function generates the file containing the contracts Abi definitions.
+     * These definitions are used to derive the types needed in the custom scaffold-eth hooks, for example.
+     * This function should be called last.
+     */
+    exportDeployments();
+  }
+
+  function test() public { }
+}
